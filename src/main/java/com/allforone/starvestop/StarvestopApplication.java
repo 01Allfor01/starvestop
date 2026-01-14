578c575ab6292c6cefe36e6fee74d657b3d234fb
d-perado <sjm01065@naver.com>
2026-01-14 09:30:11 +0900

    Initial commit

---
+++ src/main/java/com/allforone/starvestop/StarvestopApplication.java
@@ -0,0 +1,13 @@
+package com.allforone.starvestop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarvestopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarvestopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allforone/starvestop/StarvestopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allforone.starvestop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarvestopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
