ae8fa3b52d56e7e9af1348991551a7eb91e022cd
Ha-minsang <gkalstkd@gmail.com>
2026-01-15 11:45:47 +0900

    fix: 소프트 딜리트가 적용되도록 엔티티에 어노테이션 추가

--- src/main/java/com/allforone/starvestop/domain/subscription/entity/Subscription.java
+++ src/main/java/com/allforone/starvestop/domain/subscription/entity/Subscription.java
@@ -6,12 +6,14 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.math.BigDecimal;
 
 @Getter
 @Entity
 @Table(name = "subscriptions")
+@SQLRestriction("is_deleted = false")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Subscription extends BaseEntity {
 
