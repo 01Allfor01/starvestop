47e3f91f3d0e1149b6306f83dd92f97ccbe37055
Ha-minsang <gkalstkd@gmail.com>
2026-01-16 15:13:38 +0900

    fix: 핫픽스 사항들 변경 - Day 리스트에 밀타임을 받아오던 부분 수정 - Day와 MealTime에 최소 최대값 지정 - 리스폰스 바디 형식 변경

--- src/main/java/com/allforone/starvestop/domain/usersubscription/dto/request/CreateUserSubscriptionRequest.java
+++ src/main/java/com/allforone/starvestop/domain/usersubscription/dto/request/CreateUserSubscriptionRequest.java
@@ -1,5 +1,7 @@
 package com.allforone.starvestop.domain.usersubscription.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,12 @@ import lombok.NoArgsConstructor;
 public class CreateUserSubscriptionRequest {
 
     @NotNull(message = "요일을 선택 해 주세요")
+    @Min(value = 1)
+    @Max(value = 127)
     private int day;
 
     @NotNull(message = "식사 시간을 선택 해 주세요")
+    @Min(value = 1)
+    @Max(value = 7)
     private int mealTime;
 }
