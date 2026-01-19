b49de3df954eb145f85e7f63ebe5d15b14951497
d-perado <sjm01065@naver.com>
2026-01-19 09:15:04 +0900

    fix: pr 수정 안내 메세지 . 제거

--- src/main/java/com/allforone/starvestop/domain/auth/dto/request/SignInRequest.java
+++ src/main/java/com/allforone/starvestop/domain/auth/dto/request/SignInRequest.java
@@ -8,9 +8,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class SignInRequest {
-    @Email(message = "이메일 형식이 아닙니다.")
-    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식이 아닙니다")
+    @NotBlank(message = "이메일을 입력해주세요")
     private String email;
-    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
 }
