02f318365c4d76b0ed89938bd8f47528bcd59fc6
KWON-WOO <ckw075@naver.com>
2026-01-22 12:59:17 +0900

    feat: order 세팅

--- src/main/java/com/allforone/starvestop/domain/order/controller/OrderController.java
+++ src/main/java/com/allforone/starvestop/domain/order/controller/OrderController.java
@@ -1,4 +1,13 @@
 package com.allforone.starvestop.domain.order.controller;
 
+import com.allforone.starvestop.domain.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
 }
--- src/main/java/com/allforone/starvestop/domain/order/service/OrderService.java
+++ src/main/java/com/allforone/starvestop/domain/order/service/OrderService.java
@@ -1,4 +1,7 @@
 package com.allforone.starvestop.domain.order.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
 }
