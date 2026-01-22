c14154e5671cfe5ccc3a4708aa980b80cc0a2389
KWON-WOO <ckw075@naver.com>
2026-01-22 15:13:46 +0900

    feat: cart 세팅

--- src/main/java/com/allforone/starvestop/domain/cart/controller/CartController.java
+++ src/main/java/com/allforone/starvestop/domain/cart/controller/CartController.java
@@ -1,4 +1,13 @@
 package com.allforone.starvestop.domain.cart.controller;
 
+import com.allforone.starvestop.domain.cart.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class CartController {
+
+    private final CartService cartService;
+
 }
--- src/main/java/com/allforone/starvestop/domain/cart/service/CartService.java
+++ src/main/java/com/allforone/starvestop/domain/cart/service/CartService.java
@@ -1,4 +1,13 @@
 package com.allforone.starvestop.domain.cart.service;
 
+import com.allforone.starvestop.domain.cart.repository.CartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CartService {
+
+    private final CartRepository cartRepository;
+
 }
