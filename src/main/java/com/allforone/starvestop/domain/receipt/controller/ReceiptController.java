d8aad6e2293480f0b9805c208564a5c5869b36cd
d-perado <sjm01065@naver.com>
2026-01-22 14:45:56 +0900

    feat: 결제로그 상세조회,전체조회

--- src/main/java/com/allforone/starvestop/domain/receipt/controller/ReceiptController.java
+++ src/main/java/com/allforone/starvestop/domain/receipt/controller/ReceiptController.java
@@ -1,9 +1,13 @@
 package com.allforone.starvestop.domain.receipt.controller;
 
+import com.allforone.starvestop.domain.receipt.repository.ReceiptRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/receipt")
 public class ReceiptController {
+    private final ReceiptRepository receiptRepository;
 }
