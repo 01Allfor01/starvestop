28cd219e8cbc4712b49f21b5f9694c7df0e74875
KWON-WOO <ckw075@naver.com>
2026-01-22 21:32:52 +0900

    refactor: minvalue 추가

--- src/main/java/com/allforone/starvestop/domain/cart/dto/CartRequest.java
+++ src/main/java/com/allforone/starvestop/domain/cart/dto/CartRequest.java
@@ -1,5 +1,6 @@
 package com.allforone.starvestop.domain.cart.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class CartRequest {
     @NotNull(message = "상품 아이디를 입력해주세요")
     private Long productId;
+    @Min(value = 1, message = "수량이 잘못되었습니다")
     @NotNull(message = "상품 수량을 입력해주세요")
     private Integer Quantity;
 }
