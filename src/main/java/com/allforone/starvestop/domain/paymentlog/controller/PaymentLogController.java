eadc12f2641e81be7de551ef77d4bf1cc01988c1
d-perado <sjm01065@naver.com>
2026-01-22 09:55:01 +0900

    feat: 특정 유저 결제로그 조회

--- src/main/java/com/allforone/starvestop/domain/paymentlog/controller/PaymentLogController.java
+++ src/main/java/com/allforone/starvestop/domain/paymentlog/controller/PaymentLogController.java
@@ -7,6 +7,7 @@ import com.allforone.starvestop.domain.paymentlog.service.PaymentLogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.List;
 public class PaymentLogController {
     private final PaymentLogService paymentLogService;
 
+    @GetMapping
     public ResponseEntity<CommonResponse<List<GetPaymentLogResponse>>> getPaymentLogListByUserId(
             @RequestParam long userId
     ) {
