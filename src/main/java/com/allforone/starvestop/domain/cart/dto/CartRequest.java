cf7668018a8b789da58795b1322f97bcc9814dfe
KWON-WOO <ckw075@naver.com>
2026-01-22 21:16:20 +0900

    feat: 카트 리퀘스트 valid 추가

--- src/main/java/com/allforone/starvestop/domain/cart/dto/CartRequest.java
+++ src/main/java/com/allforone/starvestop/domain/cart/dto/CartRequest.java
@@ -1,11 +1,14 @@
 package com.allforone.starvestop.domain.cart.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CartRequest {
+    @NotNull(message = "상품 아이디를 입력해주세요")
     private Long productId;
+    @NotNull(message = "상품 수량을 입력해주세요")
     private Integer Quantity;
 }
