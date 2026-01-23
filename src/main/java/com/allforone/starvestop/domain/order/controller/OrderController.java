537bbf0fb4982f52b4c359c97609aecd49ee21e9
KWON-WOO <ckw075@naver.com>
2026-01-23 09:45:12 +0900

    feat: Order 중간 저장

--- src/main/java/com/allforone/starvestop/domain/order/controller/OrderController.java
+++ src/main/java/com/allforone/starvestop/domain/order/controller/OrderController.java
@@ -2,14 +2,15 @@ package com.allforone.starvestop.domain.order.controller;
 
 import com.allforone.starvestop.common.dto.AuthUser;
 import com.allforone.starvestop.common.dto.CommonResponse;
+import com.allforone.starvestop.domain.order.dto.OrderRequest;
 import com.allforone.starvestop.domain.order.dto.OrderResponse;
 import com.allforone.starvestop.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.allforone.starvestop.common.enums.SuccessMessage.ORDER_CREATE_SUCCESS;
@@ -20,9 +21,9 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/{orderId}")
-    public ResponseEntity<CommonResponse<OrderResponse>> createOrder(@AuthenticationPrincipal AuthUser authuser, @PathVariable Long orderId) {
-        OrderResponse response = orderService.createOrder(authuser.getUserId(), orderId);
+    @PostMapping
+    public ResponseEntity<CommonResponse<OrderResponse>> createOrder(@AuthenticationPrincipal AuthUser authuser, @RequestBody OrderRequest request) {
+        OrderResponse response = orderService.createOrder(authuser.getUserId(), request.getStoreId());
         return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success(ORDER_CREATE_SUCCESS, response));
     }
 }
--- src/main/java/com/allforone/starvestop/domain/order/service/OrderService.java
+++ src/main/java/com/allforone/starvestop/domain/order/service/OrderService.java
@@ -1,17 +1,36 @@
 package com.allforone.starvestop.domain.order.service;
 
+import com.allforone.starvestop.common.exception.CustomException;
+import com.allforone.starvestop.common.exception.ErrorCode;
 import com.allforone.starvestop.domain.order.dto.OrderResponse;
 import com.allforone.starvestop.domain.order.repository.OrderRepository;
+import com.allforone.starvestop.domain.store.entity.Store;
+import com.allforone.starvestop.domain.store.repository.StoreRepository;
+import com.allforone.starvestop.domain.user.entity.User;
+import com.allforone.starvestop.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
 
-    public OrderResponse createOrder(Long userId, Long orderId) {
+    public OrderResponse createOrder(Long userId, Long storeId) {
+        User user = userRepository.findByIdAndIsDeletedIsFalse(userId).orElseThrow(
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        Store store = storeRepository.findByIdAndIsDeletedIsFalse(storeId).orElseThrow(
+                () -> new CustomException(ErrorCode.STORE_NOT_FOUND));
+
+        String orderKey = UUID.randomUUID().toString();
+
+
         return null;
     }
 }
