f5a96fd3ed748edd27b3596a96d7b82f04a9f0a7
LEE SEOJUN <junbeom95@gmail.com>
2026-01-23 09:38:41 +0900

    refactor: 사용자 구독 API 검토
    1. 오타 수정

--- src/main/java/com/allforone/starvestop/common/enums/SuccessMessage.java
+++ src/main/java/com/allforone/starvestop/common/enums/SuccessMessage.java
@@ -18,9 +18,9 @@ public enum SuccessMessage {
     OWNER_DELETE_SUCCESS("회원 탈퇴 성공"),
 
     //사용자 구독
-    USER_SUBSCRIPTION_CREATE_SUCCESS("사용자 구독 생성 성공"),
-    USER_SUBSCRIPTION_GET_SUCCESS("사용자 구독 조회 성공"),
-    USER_SUBSCRIPTION_CANCEL_SUCCESS("사용자 구독 취소 성공"),
+    USER_SUBSCRIPTION_CREATE_SUCCESS("구독 성공"),
+    USER_SUBSCRIPTION_GET_SUCCESS("구독 조회 성공"),
+    USER_SUBSCRIPTION_CANCEL_SUCCESS("구독 취소 성공"),
 
 
     //상품
