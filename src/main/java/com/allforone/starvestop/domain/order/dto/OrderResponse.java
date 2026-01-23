e25bb557b940dcb625e6c851cc108649dc378cf5
KWON-WOO <ckw075@naver.com>
2026-01-23 19:09:45 +0900

    refactor : OrderResponse 필드명 orderId -> id로 수정

--- src/main/java/com/allforone/starvestop/domain/order/dto/OrderResponse.java
+++ src/main/java/com/allforone/starvestop/domain/order/dto/OrderResponse.java
@@ -1,26 +1,25 @@
 package com.allforone.starvestop.domain.order.dto;
 
 import com.allforone.starvestop.domain.order.enums.OrderStatus;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 public class OrderResponse {
-    private final Long orderId;
+    private final Long id;
     private final Long storeId;
     private final Long userId;
     private final String orderKey;
-    private final OrderStatus orderStatus;
+    private final OrderStatus status;
     private final BigDecimal amount;
 
     public OrderResponse(Long orderId, Long storeId, Long userId, String orderKey, OrderStatus orderStatus, BigDecimal amount) {
-        this.orderId = orderId;
+        this.id = orderId;
         this.storeId = storeId;
         this.userId = userId;
         this.orderKey = orderKey;
-        this.orderStatus = orderStatus;
+        this.status = orderStatus;
         this.amount = amount;
     }
 }
