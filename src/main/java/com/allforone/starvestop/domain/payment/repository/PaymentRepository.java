f959142a9ea971e946d0f2fafee0b32dffc5bf36
d-perado <sjm01065@naver.com>
2026-01-29 20:38:56 +0900

    fix: pr내용 수정

--- src/main/java/com/allforone/starvestop/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/allforone/starvestop/domain/payment/repository/PaymentRepository.java
@@ -1,7 +1,6 @@
 package com.allforone.starvestop.domain.payment.repository;
 
 import com.allforone.starvestop.domain.payment.entity.Payment;
-import com.allforone.starvestop.domain.payment.enums.PaymentStatus;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -12,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long>, PaymentRepositoryCustom {
-    Optional<Payment> findByIdAndIsDeletedIsFalse(Long userId);
+    Optional<Payment> findByIdAndIsDeletedIsFalse(Long paymentId);
 
     Optional<Payment> findPaymentByOrderKey(String orderOrderKey);
 
