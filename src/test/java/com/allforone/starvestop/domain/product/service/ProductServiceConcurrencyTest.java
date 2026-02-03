45197573fbdc621b7df3cf2a96ed1b231e846454
KWON-WOO <ckw075@naver.com>
2026-02-03 20:40:14 +0900

    feat: 테스트코드 어노테이션 제거

--- src/test/java/com/allforone/starvestop/domain/product/service/ProductServiceConcurrencyTest.java
+++ src/test/java/com/allforone/starvestop/domain/product/service/ProductServiceConcurrencyTest.java
@@ -29,8 +29,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Tag("prductConcurrency")
-@ActiveProfiles("testProductConcurrency")
+
 class ProductServiceConcurrencyTest {
 
     @Autowired
