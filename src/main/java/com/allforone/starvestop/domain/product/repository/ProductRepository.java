2faa8957e353f9aec3c4c80ca544cc8bbc005691
LEE SEOJUN <junbeom95@gmail.com>
2026-02-11 16:08:24 +0900

    chore: merge 충돌 해결

--- src/main/java/com/allforone/starvestop/domain/product/repository/ProductRepository.java
+++ src/main/java/com/allforone/starvestop/domain/product/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.allforone.starvestop.domain.product.repository;
 
 import com.allforone.starvestop.domain.product.dto.ProductSaleDto;
 import com.allforone.starvestop.domain.product.entity.Product;
+import com.allforone.starvestop.domain.product.enums.ProductStatus;
 import com.allforone.starvestop.domain.store.enums.StoreCategory;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
