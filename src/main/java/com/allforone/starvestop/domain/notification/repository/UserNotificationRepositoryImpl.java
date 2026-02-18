bbd25d41c4e88381ed53d24ebe22cbe8a4ec8eaf
KWON-WOO <ckw075@naver.com>
2026-02-18 18:31:29 +0900

    refactor: 당일 알림 발신할 데이터 미리 적재하도록 중간 테이블 생성

--- src/main/java/com/allforone/starvestop/domain/notification/repository/UserNotificationRepositoryImpl.java
+++ src/main/java/com/allforone/starvestop/domain/notification/repository/UserNotificationRepositoryImpl.java
@@ -1,14 +1,18 @@
 package com.allforone.starvestop.domain.notification.repository;
 
 import com.allforone.starvestop.domain.notification.entity.UserNotification;
+import com.allforone.starvestop.domain.subscription.enums.UserSubscriptionStatus;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import static com.allforone.starvestop.domain.notification.entity.QUserNotification.userNotification;
 import static com.allforone.starvestop.domain.order.entity.QOrder.order;
-import static com.allforone.starvestop.domain.store.entity.QStore.store;
 import static com.allforone.starvestop.domain.owner.entity.QOwner.owner;
+import static com.allforone.starvestop.domain.store.entity.QStore.store;
+import static com.allforone.starvestop.domain.subscription.entity.QUserSubscription.userSubscription;
 
 
 @Repository
@@ -30,6 +34,18 @@ public class UserNotificationRepositoryImpl implements UserNotificationRepositor
                         owner.isDeleted.isFalse()
                 ).fetchOne();
 
+    }
+
+    @Override
+    public List<String> findUserTokenBySubscriptionId(Long subscriptionId) {
+        return queryFactory
+                .select(userNotification.token)
+                .from(userSubscription)
+                .join(userNotification).on(userNotification.userId.eq(userSubscription.user.id))
+                .where(
+                        userSubscription.subscription.id.eq(subscriptionId),
+                        userSubscription.status.eq(UserSubscriptionStatus.ACTIVE)
+                ).fetch();
     }
 
 //    @Override
