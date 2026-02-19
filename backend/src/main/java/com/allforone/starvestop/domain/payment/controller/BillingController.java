ef8e00f332541c0b7af5b4605b83f8fe427c85cc
d-perado <sjm01065@naver.com>
2026-02-19 22:57:55 +0900

    feat : 결제 수단 조회기능

--- backend/src/main/java/com/allforone/starvestop/domain/payment/controller/BillingController.java
+++ backend/src/main/java/com/allforone/starvestop/domain/payment/controller/BillingController.java
@@ -6,6 +6,7 @@ import com.allforone.starvestop.common.dto.AuthUser;
 import com.allforone.starvestop.common.dto.CommonResponse;
 import com.allforone.starvestop.domain.payment.dto.request.BillingConfirmRequest;
 import com.allforone.starvestop.domain.payment.dto.response.BillingConfirmResponse;
+import com.allforone.starvestop.domain.payment.dto.response.GetBillingKeyResponse;
 import com.allforone.starvestop.domain.payment.service.BillingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -14,12 +15,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.allforone.starvestop.common.enums.SuccessMessage.BILLING_CONFIRM_SUCCESS;
+import static com.allforone.starvestop.common.enums.SuccessMessage.BILLING_GET_SUCCESS;
 
 @Tag(name = "Billing", description = "정기결제(빌링) API")
 @SecurityRequirement(name = OpenApiConfig.BEARER)
@@ -45,5 +44,16 @@ public class BillingController {
         );
 
         return ResponseEntity.ok(CommonResponse.success(BILLING_CONFIRM_SUCCESS, new BillingConfirmResponse(true)));
+    }
+
+    @Operation(summary = "정기 결제 수단 조회" + ApiRoleLabels.USER)
+    @GetMapping
+    public ResponseEntity<CommonResponse<GetBillingKeyResponse>> getMyBillingKey(
+            @AuthenticationPrincipal AuthUser authUser
+    ) {
+        Long userId = authUser.getUserId();
+        GetBillingKeyResponse response = billingService.getBillingKey(userId);
+
+        return ResponseEntity.ok(CommonResponse.success(BILLING_GET_SUCCESS, response));
     }
 }
